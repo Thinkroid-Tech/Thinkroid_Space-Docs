276f73ece90b0a12a62295af202da641308baf6b
Thinkroid <git@thinkroid.com>
2026-04-03 08:20:43 -0700

    Remove custom domain, use default GitHub Pages URL

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 export default {
   title: 'Thinkroid Space',
   description: 'Documentation for the gamified multi-agent orchestration platform',
-  base: '/',
+  base: '/Thinkroid_Space-Docs/',
   ignoreDeadLinks: [/localhost/],
   head: [
     ['link', { rel: 'icon', href: '/logo.png' }]
